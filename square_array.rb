a3b438c85108dd2271f290fe9e0204d6f7a5672f
bruce willis <fashun_vyctum@yahoo.com>
2020-03-30 23:58:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
   new_numbers = array.new[]
  array.each do |numbers|
-   new_numbers << #{numbers} ** 2
+   new_numbers << numbers ** 2
  
   end
-  
+  new_numbers
 end
 
 
